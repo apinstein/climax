6b6573e96ba03d431fb402a3995d542b0bb99a98
Alan Pinstein <apinstein@mac.com>
2011-12-24 14:16:06 -0500

    factor out templates to improve readability

---
+++ templates/app_scaffold.php
@@ -0,0 +1,25 @@
+#!/usr/bin/env php
+<?php echo "<?php\n"; ?>
+/* vim: set syntax=php expandtab tabstop=4 shiftwidth=4: */
+/**
+ * Created by CLImax http://github.com/apinstein/climax
+ * <?php echo date('r'); ?>
+ */
+
+require_once "CLImax.php";
+class CLImaxSampleCommand extends CLIMax_BaseCommand
+{
+    public function run($arguments, CLImaxController $cliController)
+    {
+        // do something interesting
+        print "Sample...\n";
+        // or throw new Exception("error", $returnCode);
+        return 0;
+    }
+}
+
+// WIRE UP APPLICTION
+CLImaxController::create()
+                  ->addCommand(new CLImaxSampleCommand, array("sample"))
+                  ->run($argv, $argc);
+
